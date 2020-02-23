b808d7e726f5dabcd5ad6b4e041aa4ddfd54e0e0
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-23 07:30:03 +0100

    feat(client): add components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,9 +3,13 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { RegisterComponent } from './register/register.component';
+import { ProfileComponent } from './profile/profile.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, RegisterComponent, ProfileComponent, LoginComponent, HomeComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]

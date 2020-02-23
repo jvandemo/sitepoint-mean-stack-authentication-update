2b9733945aa5427cbc220b8b79db36204ba92561
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-23 06:20:54 +0100

    feat(app): fix routing to ensure next routes are triggered

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(function(req, res, next) {
 // error handlers
 
 // [SH] Catch unauthorised errors
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   if (err.name === "UnauthorizedError") {
     res.status(401);
     res.json({ message: err.name + ": " + err.message });
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get("env") === "development") {
-  app.use(function(err, req, res, next) {
+  app.use(function(err, req, res) {
     res.status(err.status || 500);
     res.render("error", {
       message: err.message,
@@ -73,7 +73,7 @@ if (app.get("env") === "development") {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function(err, req, res) {
   res.status(err.status || 500);
   res.render("error", {
     message: err.message,

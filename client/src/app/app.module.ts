37ba0d8e6d91d217e176f6829ae444859ddbb55f
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-23 07:59:35 +0100

    feat(client): add components logic and auth guard

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from "@angular/core";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { RegisterComponent } from './register/register.component';
-import { ProfileComponent } from './profile/profile.component';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from "./register/register.component";
+import { ProfileComponent } from "./profile/profile.component";
+import { LoginComponent } from "./login/login.component";
+import { HomeComponent } from "./home/home.component";
 
 @NgModule({
-  declarations: [AppComponent, RegisterComponent, ProfileComponent, LoginComponent, HomeComponent],
+  declarations: [
+    AppComponent,
+    RegisterComponent,
+    ProfileComponent,
+    LoginComponent,
+    HomeComponent
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]

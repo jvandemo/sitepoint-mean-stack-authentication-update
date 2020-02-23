5f56b4f460ba7afc69a7cfcb90831da1bfb49ccb
Jurgen Van de Moere <jurgen.van.de.moere@gmail.com>
2020-02-23 06:20:19 +0100

    feat(routes): update syntax

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,19 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var jwt = require('express-jwt');
+var jwt = require("express-jwt");
 var auth = jwt({
-  secret: 'MY_SECRET',
-  userProperty: 'payload'
+  secret: "MY_SECRET",
+  userProperty: "payload"
 });
 
-var ctrlProfile = require('../controllers/profile');
-var ctrlAuth = require('../controllers/authentication');
+var ctrlProfile = require("../controllers/profile");
+var ctrlAuth = require("../controllers/authentication");
 
 // profile
-router.get('/profile', auth, ctrlProfile.profileRead);
+router.get("/profile", auth, ctrlProfile.profileRead);
 
 // authentication
-router.post('/register', ctrlAuth.register);
-router.post('/login', ctrlAuth.login);
+router.post("/register", ctrlAuth.register);
+router.post("/login", ctrlAuth.login);
 
 module.exports = router;
